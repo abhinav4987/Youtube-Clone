c2400fa0c961ad97da26548d5b733da63bcd1282
abhinav4987 <abhinavanand4987@gmail.com>
2021-11-23 19:43:23 +0530

    tweaks

--- youtube-clone-react-redux-main/src/redux/store.js
+++ youtube-clone-react-redux-main/src/redux/store.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
